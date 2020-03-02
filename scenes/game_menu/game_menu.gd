384278a8eb0fa61c04742e72d9fca78cb6156498
PJTSearch <pjtsignups@gmail.com>
2020-03-01 18:00:22 -0800

    add game menu

---
+++ scenes/game_menu/game_menu.gd
@@ -0,0 +1,23 @@
+extends PopupPanel
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _unhandled_input(event):
+	if event is InputEventKey:
+		if event.pressed and event.scancode == KEY_ESCAPE:
+			if !visible:
+				popup_centered()
+			else:
+				visible = false
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
