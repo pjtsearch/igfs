79b02fb076c9370214ef79f3949d4c20517b2000
PJTSearch <pjtsignups@gmail.com>
2020-03-16 12:40:46 -0700

    start adding console

---
+++ scenes/HUD/console.gd
@@ -0,0 +1,20 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+onready var console = $Console
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+#func _unhandled_input(event):
+#	if event is InputEventKey:
+#		if event.pressed and event.scancode == 96:
+#			console.show()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
