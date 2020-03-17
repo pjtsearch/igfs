02fbb396aa8f58cca97154f165b397eff6b326af
PJTSearch <pjtsignups@gmail.com>
2020-03-16 18:23:16 -0700

    sync multiplayer console messages

--- scenes/HUD/console.gd
+++ scenes/HUD/console.gd
@@ -9,6 +9,11 @@ onready var console = $Console
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	console.connect("visibility_changed",self,"visibility_changed")
+	console.userName = network.my_info.name
+	
+	var say_ref = CommandRef.new(self, "say", CommandRef.VARIANT)
+	var say_command = ConsoleCommand.new('say', say_ref , 'Say to other players.')
+	console.add_command(say_command)
 	
 func visibility_changed():
 	if console.visible:
@@ -19,6 +24,19 @@ func visibility_changed():
 #		get_tree().paused = false
 		pause_handler.resume_movement()
 
+remote func write_message(name,message):
+	console.write_line("")
+	console.write_line(format_message(name,message))
+	
+func say(args):
+	var message = PoolStringArray(args).join(" ")
+	console.write_line("")
+	console.write_line(format_message(network.my_info.name,message))
+#	if console.selectedChannel == "All":
+	rpc("write_message",network.my_info.name,message)
+	
+func format_message(name,message):
+	return console.get_time_stamp(false)+"> [color=#0000ff]"+name+ "[/color]: "+message
 
 #func _unhandled_input(event):
 #	if event is InputEventKey:
